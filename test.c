d02a0db04bfdedc609386b70f0e195692db410e2
Omkar-Irappanavar <137825836+Omkar-Irappanavar@users.noreply.github.com>
2025-05-23 17:50:22 +0530

    Add files via upload

---
+++ test.c
@@ -0,0 +1,40 @@
+// /* this is multi-line comment */
+// single line comment
+
+
+#include<stdio.h>
+#include "s2html_event.h"
+
+#define ADD(a,b) (a+b+10)
+
+#define length 20
+
+struct student
+{
+    int num;
+    double var;
+};
+
+int x = 'a';
+    
+int main()
+{
+    int n_ew23 = 10;
+    float *ptr = 2;        // hey bro
+    #ifndef length
+    {
+        printf("Hi\n");
+    }
+    #endif
+    for(int i=0; i<5; i++)
+    {
+        for(int j=0;j<4;j++)
+        {
+            printf("Hi\n");
+            num=num/10;
+            break;
+        }
+    }
+    char ch = ch << 'A';          /* mister by */
+    char *str = "hello";
+}
